301e250a5989d3030697f002c5ea8508481ba4d6
Fredrik Söderström <fredrik.soderstrom@cambio.se>
2017-11-30 22:21:59 +0100

    Experimenting with matching mips

--- linux-arm64/Toolchain.cmake
+++ linux-arm64/Toolchain.cmake
@@ -3,13 +3,15 @@ set(CMAKE_SYSTEM_VERSION 1)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 set(cross_triple "aarch64-linux-gnu")
+set(cross_root /usr/xcc/${cross_triple})
 
-set(CMAKE_C_COMPILER /usr/bin/${cross_triple}-cc)
-set(CMAKE_CXX_COMPILER /usr/bin/${cross_triple}-c++)
-set(CMAKE_Fortran_COMPILER /usr/bin/${cross_triple}-gfortran)
+set(CMAKE_C_COMPILER ${cross_root}/bin/${cross_triple}-gcc)
+set(CMAKE_CXX_COMPILER ${cross_root}/bin/${cross_triple}-g++)
+set(CMAKE_Fortran_COMPILER ${cross_root}/bin/${cross_triple}-gfortran)
 
-# Discard path returned by pkg-config and associated with HINTS in module
-# like FindOpenSSL.
-set(CMAKE_IGNORE_PATH /usr/lib/x86_64-linux-gnu/ /usr/lib/x86_64-linux-gnu/lib/)
+set(CMAKE_FIND_ROOT_PATH ${cross_root}/lib ${cross_root}/${cross_triple}/lib)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 set(CMAKE_CROSSCOMPILING_EMULATOR /usr/bin/qemu-aarch64)
